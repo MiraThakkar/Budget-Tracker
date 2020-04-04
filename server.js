4d65bc664c38d75b06f83c78250d75a2603b9a7b
Mira Thakkar <mthakkar84@gmail.com>
2020-04-04 18:55:02 -0400

    added provcess env var for heroky deployment

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const compression = require("compression");
+// const compression = require("compression");
 const path = require("path");
-
-const PORT = 3000;
 
 const app = express();
 
 app.use(logger("dev"));
 
-app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
+const databaseUrl = process.env.MONGODB_URI || "budget";
+const collections = ["transactions"];
 
-// routes
+const db = mongojs(databaseUrl, collections);
+
+db.on("error", error => {
+  console.log("Database Error:", error);
+});
 app.use(require("./routes/api.js"));
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
@@ -30,37 +31,3 @@ app.listen(PORT, () => {
 
 
 
-
-
-
-// const express = require("express");
-// const mongojs = require("mongojs");
-// const mongoose = require("mongoose");
-// const logger = require("morgan");
-// const path = require("path");
-
-// const app = express();
-
-// app.use(logger("dev"));
-
-// app.use(express.urlencoded({ extended: true }));
-// app.use(express.json());
-
-// app.use(express.static("public"));
-
-// const databaseUrl = process.env.MONGODB_URI || "budget";
-// const collections = ["transactions"];
-
-// const db = mongojs(databaseUrl, collections);
-
-// db.on("error", error => {
-//   console.log("Database Error:", error);
-// });
-
-// app.use(require("./routes/api.js"));
-
-// const PORT = process.env.PORT || 3000;
-
-// app.listen(PORT, () => {
-//   console.log(`Application running on PORT ${PORT}`);
-// });
