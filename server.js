e9a4b24f6971cc4231689488162d0ea6b5bcd59e
Mira Thakkar <mthakkar84@gmail.com>
2020-04-04 20:06:20 -0400

    changed serve.js to connect to budget database

--- server.js
+++ server.js
@@ -1,18 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-// const compression = require("compression");
+const compression = require("compression");
 const path = require("path");
 
 const app = express();
-
-var MONGODB_URI = process.env.MONGODB_URL || "mongodb://localhost/budget";
-const options = {
-  useNewUrlParser: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-  family: 4 // Use IPv4, skip trying IPv6
-};
 
 
 app.use(logger("dev"));
@@ -22,7 +14,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(MONGODB_URI,options)
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
+  useNewUrlParser: true
+});
 
 app.use(require("./routes/api.js"));
 
